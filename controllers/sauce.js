b10e57091e51525088a587836cc7c1e9bb3a0d87
Jean-Pierre <jp0065@hotmail.com>
2021-01-01 11:36:13 +0100

    correction bug controllers sauce modifySauce suppression de la veille image

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -41,11 +41,29 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
+  /*si on modifie le fichier image, récupérer le nom du fichier image sauce actuelle pour la suppréssion
+  pour éviter d'avoir un fichier inutile dans le dossier images */
+  if(req.file){
+    Sauce.findOne({ _id: req.params.id})
+    .then(sauce => {
+      const filename = sauce.imageUrl.split("/images")[1];
+      //suppression de l'image de la sauce car elle va être remplacer par la nouvelle image de sauce
+      fs.unlink(`images/${filename}`, (err) => {
+        if(err) throw err;
+      })
+    })
+    .catch(error => res.status(400).json({error}));  
+  }else{};
+
+  //l'objet qui va être envoyé dans la base de donnée
   const sauceObject = req.file ?
   {
     ...JSON.parse(req.body.sauce),
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  } : { ...req.body};
+  } :
+  { ...req.body};
+
+  //update dans la base de donnée
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id }) //2 arguments l'objet et la nouvelle version de l'objet
     .then(() => res.status(200).json({ message: "objet mise à jour" }))
     .catch((error) => res.status(404).json({ error }));
